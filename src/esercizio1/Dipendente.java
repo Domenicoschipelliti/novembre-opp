59f0bcfd453c57fea53b7d3edfbc4339fda1d08e
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2023-11-30 16:45:32 +0100

    esercizi

--- src/esercizio1/Dipendente.java
+++ src/esercizio1/Dipendente.java
@@ -2,9 +2,9 @@ package esercizio1;
 
 
 public class Dipendente {
-     protected static int stipendioBase;
+     public static double stipendioBase=1000;
      protected static String matricola;
-     protected static int stipendio;
+     protected static double stipendio;
 
      private static int importoOrarioStraordinario;
 
@@ -22,10 +22,17 @@ public class Dipendente {
         VENDITE
     }
 
-     public  int lavoro(String matricola,String Dipartimento){
+     public  double lavoro(String matricola,String Dipartimento){
           stipendio=stipendioBase;
           importoOrarioStraordinario=30;
+          String mat=matricola;
+          String di=Dipartimento;
+          System.out.println("Sarai la matricola  "+ mat);
+          System.out.println("Lavorerai nel dipartimento  " + di);
           Livello livello1 =  Livello.OPERAIO;
+          System.out.println("Partirà da grado  "+ livello1);
+          System.out.println("Per ogni straordinario piglia  " + importoOrarioStraordinario + "  Euro"  );
+          return stipendio;
 
      }
 
@@ -43,11 +50,36 @@ public class Dipendente {
 
      }
 
+     public Livello promuovi(){
+        Livello impiegato=Livello.IMPIEGATO;
+        if (Livello.OPERAIO!=impiegato){
+           System.out.println("complimenti sei stato promosso ad  "+ Livello.IMPIEGATO);
+        }else {
+            System.out.println("niente");
+        }
 
-     public static void  stampaDatiDipendente(){
-         System.out.println(matricola);
-         System.out.println(stipendio);
+        return impiegato; //meglio uno switch
+
+
+
      }
+
+
+
+     public float aggiorna(float i){
+         Livello impiegato=Livello.IMPIEGATO;
+
+
+
+         if (impiegato==Livello.IMPIEGATO){
+           System.out.println(stipendioBase*i);
+         }else {
+             System.out.println("nada");
+         }
+         return i;
+     }
+
+
 
 
 }
--- src/esercizio1/Main.java
+++ src/esercizio1/Main.java
@@ -2,6 +2,9 @@ package esercizio1;
 
 public class Main {
     public static void main(String[] args) {
-
+         Dipendente area=new Dipendente();
+         System.out.println(area.lavoro("289829","produttivo"));
+         area.promuovi();
+         area.aggiorna(12);
     }
 }
